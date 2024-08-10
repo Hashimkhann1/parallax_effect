0975834d9fa8156f82971742d0c95fd729942ce8
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-08-10 22:01:58 +0500

    view model

---
+++ lib/view_model/notification_view_model/notification_view_model.dart
@@ -0,0 +1,33 @@
+
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationViewModel {
+
+  // FirebaseMessaging messaging = Fire
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+
+  /// request notification permission
+  void requestNotificationPermission() async {
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: true,
+      badge: true,
+      carPlay: true,
+      criticalAlert: true,
+      provisional: true,
+      sound: true,
+    );
+
+    if(settings.authorizationStatus == AuthorizationStatus.authorized){
+      print("User granyted permission");
+    }else if(settings.authorizationStatus == AuthorizationStatus.provisional) {
+      print("User granyted provisional permission");
+    }else{
+      print("User denied permission");
+    }
+
+  }
+}
