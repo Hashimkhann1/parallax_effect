b79087a4e722137f3267e53d7a2b057d74163aa1
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-08-11 22:28:07 +0500

    botification view

---
+++ lib/view/notification_home_view/notification_home_view.dart
@@ -0,0 +1,40 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:parallax_scroll/utils/widgets/my_text.dart';
+import 'package:parallax_scroll/view_model/notification_view_model/notification_view_model.dart';
+
+class NotificationHomeView extends StatefulWidget {
+  const NotificationHomeView({super.key});
+
+  @override
+  State<NotificationHomeView> createState() => _NotificationHomeViewState();
+}
+
+class _NotificationHomeViewState extends State<NotificationHomeView> {
+
+  NotificationViewModel notificationViewModel = NotificationViewModel();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    notificationViewModel.requestNotificationPermission();
+
+    notificationViewModel.isTokenRefresh();
+    notificationViewModel.getDeviceToken().then((value) {
+      print("token ");
+      print(value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: MyText(title: "This is notification home screen",fontSize: 20,),
+      ),
+    );
+  }
+}
