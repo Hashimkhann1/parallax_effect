1989f20262ae71daf817276526aa8dffdcbd0a26
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-08-12 22:35:45 +0500

    new mwthods is added for notifications

--- lib/view_model/notification_view_model/notification_view_model.dart
+++ lib/view_model/notification_view_model/notification_view_model.dart
@@ -1,12 +1,122 @@
 
 
+import 'dart:io';
+import 'dart:math';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:parallax_scroll/view/message_screen_view/message_screen_view.dart';
 
 class NotificationViewModel {
 
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   // FirebaseMessaging messaging = Fire
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
+
+  void initLocalNotification(BuildContext context , RemoteMessage message) async {
+
+    var androidInitializationSettings = const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iosInitializationSettings = const DarwinInitializationSettings();
+
+    var initializationSetting = InitializationSettings(
+      android: androidInitializationSettings,
+      iOS: iosInitializationSettings,
+    );
+
+    await _flutterLocalNotificationsPlugin.initialize(
+        initializationSetting,
+      onDidReceiveNotificationResponse: (payload) {
+          handleMessage(context, message);
+      }
+    );
+
+  }
+
+
+  void firebaseInit(BuildContext context) {
+    FirebaseMessaging.onMessage.listen((message) {
+      
+      if (kDebugMode) {
+        print(message.notification!.title.toString());
+        print(message.notification!.body.toString());
+        print(message.data['type']);
+        print(message.data['id']);
+      }
+
+      if(Platform.isAndroid){
+        initLocalNotification(context, message);
+        showNotification(message);
+      }else{
+        showNotification(message);
+      }
+
+    });
+  }
+
+  Future<void> showNotification(RemoteMessage message) async {
+
+    AndroidNotificationChannel channel = AndroidNotificationChannel(
+        Random.secure().nextInt(100000).toString(),
+        "High Importance Notification",
+      importance: Importance.max
+    );
+
+    AndroidNotificationDetails androidNotificationDetails = AndroidNotificationDetails(
+        channel.id.toString(),
+        channel.name.toString(),
+      channelDescription: "Your channel Description",
+      importance: Importance.high,
+      priority: Priority.high,
+      ticker: 'ticker',
+      icon: '@mipmap/ic_launcher'
+    );
+
+    const DarwinNotificationDetails darwinNotificationDetails = DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true
+    );
+
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+      iOS: darwinNotificationDetails
+    );
+
+    Future.delayed(Duration.zero , () {
+      _flutterLocalNotificationsPlugin.show(
+          0,
+          message.notification!.title.toString(),
+          message.notification!.body.toString(),
+          notificationDetails);
+    });
+  }
+
+  Future<void> setupInteractMessage(BuildContext context) async {
+    RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+
+
+    /// this for when ap is terminated
+    if(initialMessage != null){
+      handleMessage(context, initialMessage);
+    }
+
+    /// when app is in background
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {
+      handleMessage(context, event);
+    });
+
+  }
+
+  /// below method is for when app is forground and recive notification to reqirect the user to the defing scrren
+  void handleMessage(BuildContext context , RemoteMessage message) {
+    if(message.data['type'] == 'message'){
+      Navigator.push(context, MaterialPageRoute(builder: (context) => MessageScreenView()));
+    }
+  }
 
   /// request notification permission
   void requestNotificationPermission() async {
@@ -22,11 +132,17 @@ class NotificationViewModel {
     );
 
     if(settings.authorizationStatus == AuthorizationStatus.authorized){
-      print("User granyted permission");
+      if (kDebugMode) {
+        print("User granted permission");
+      }
     }else if(settings.authorizationStatus == AuthorizationStatus.provisional) {
-      print("User granyted provisional permission");
+      if (kDebugMode) {
+        print("User granted provisional permission");
+      }
     }else{
-      print("User denied permission");
+      if (kDebugMode) {
+        print("User denied permission");
+      }
     }
 
   }
