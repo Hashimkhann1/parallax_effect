304ff923e0311fde2e03df891d7870d691b35f0b
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-08-12 22:34:34 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:parallax_scroll/firebase_options.dart';
 import 'package:parallax_scroll/view/home_view.dart';
@@ -9,7 +10,16 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
+}
+
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print(message.notification!.title.toString());
 }
 
 class MyApp extends StatelessWidget {
