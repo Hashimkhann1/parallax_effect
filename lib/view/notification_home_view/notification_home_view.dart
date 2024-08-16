670b1d1cfb1265ea44dc82cca4a4e2ce7c6fd11d
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-08-16 22:04:23 +0500

    Revert "firebase is added"
    This reverts commit c888e6b261dcae46a6c3883fb75afdd589084e49.

--- lib/view/notification_home_view/notification_home_view.dart
+++ lib/view/notification_home_view/notification_home_view.dart
@@ -1,7 +1,6 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:parallax_scroll/res/services/get_server_key.dart';
 import 'package:parallax_scroll/utils/widgets/my_text.dart';
 import 'package:parallax_scroll/view_model/notification_view_model/notification_view_model.dart';
 
@@ -15,7 +14,6 @@ class NotificationHomeView extends StatefulWidget {
 class _NotificationHomeViewState extends State<NotificationHomeView> {
 
   NotificationViewModel notificationViewModel = NotificationViewModel();
-  GetServerKey getServerKey = GetServerKey();
 
   @override
   void initState() {
@@ -24,16 +22,12 @@ class _NotificationHomeViewState extends State<NotificationHomeView> {
     notificationViewModel.requestNotificationPermission();
     notificationViewModel.firebaseInit(context);
     notificationViewModel.setupInteractMessage(context);
-    //
-    // // notificationViewModel.isTokenRefresh();
-    // notificationViewModel.getDeviceToken().then((value) {
-    //   print("token ");
-    //   print(value);
-    // });
 
-    notificationViewModel.requestNotificationPermission();
-    notificationViewModel.getDeviceToken();
-
+    // notificationViewModel.isTokenRefresh();
+    notificationViewModel.getDeviceToken().then((value) {
+      print("token ");
+      print(value);
+    });
   }
 
   @override
@@ -41,20 +35,7 @@ class _NotificationHomeViewState extends State<NotificationHomeView> {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        child: Column(
-          children: [
-            MyText(title: "This is notification home screen",fontSize: 20,),
-            TextButton(
-                onPressed: () async {
-                  String accessToken = await getServerKey.getServerKeyToken();
-                  print(">>>>>>>>>>>>>>");
-                  print(accessToken);
-                  print(">>>>>>>>>>>>>>");
-                },
-                child: MyText(title: "Check",fontSize: 30,))
-          ],
-        ),
-        
+        child: MyText(title: "This is notification home screen",fontSize: 20,),
       ),
     );
   }
